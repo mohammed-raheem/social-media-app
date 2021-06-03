0eae9fb033224906cea893433a2c05137f01909d
mohammed <mohammed@mohammeds-MacBook-Air.local>
2021-06-03 04:12:13 +0300

    edit home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
-import {useImmer} from 'use-immer'
-import Axios from "axios"
-import {Link} from "react-router-dom"
+import { useImmer } from "use-immer";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
 import StateContext from "../StateContext";
 import Page from "./Page";
@@ -9,23 +9,24 @@ import LoadingIcon from "./LoadingIcon";
 
 function Home() {
   const { user } = useContext(StateContext);
-  
+
   const [state, setState] = useImmer({
     isLoading: true,
     feed: [],
-  })
+  });
 
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
 
     async function fetchData() {
       try {
-        const response = await Axios.post('/getHomeFeed',
+        const response = await Axios.post(
+          "/getHomeFeed",
           { token: user.token },
           { cancelToken: ourRequest.token }
         );
         setState((draft) => {
-          draft.isLoading = false
+          draft.isLoading = false;
           draft.feed = response.data;
         });
       } catch (e) {
@@ -39,53 +40,54 @@ function Home() {
   }, []);
 
   if (state.isLoading) {
-    return <LoadingIcon />
+    return <LoadingIcon />;
   }
 
   return (
     <Page title="Home">
-    {state.feed.length > 0 && (
-      <>
-        <h2 className="text-center mb-4">Latest post from your community</h2>
-        <div className="list-group">
-          {state.feed.map((result) => {
-                  const date = new Date();
-                  const formattedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+      {state.feed.length > 0 && (
+        <>
+          <h2 className="text-center mb-4">Latest posts from your community</h2>
+          <div className="list-group">
+            {state.feed.map((result) => {
+              const date = new Date();
+              const formattedDate = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 
-                  return (
-                    <Link
-                      to={`/post/${result._id}`}
-                      key={result._id}
-                      className="list-group-item list-group-item-action"
-                    >
-                      <img
-                        className="avatar-tiny"
-                        src={result.author.avatar}
-                        alt=""
-                      />{" "}
-                      <strong>{result.title} </strong>
-                      <span className="text-muted small">
-                        by brad on {formattedDate}{" "}
-                      </span>
-                    </Link>
-                  );
-                })}
-        </div>
-      </>
-    )}
+              return (
+                <Link
+                  to={`/post/${result._id}`}
+                  key={result._id}
+                  className="list-group-item list-group-item-action"
+                >
+                  <img
+                    className="avatar-tiny"
+                    src={result.author.avatar}
+                    alt=""
+                  />{" "}
+                  <strong>{result.title} </strong>
+                  <span className="text-muted small">
+                    by brad on {formattedDate}{" "}
+                  </span>
+                </Link>
+              );
+            })}
+          </div>
+        </>
+      )}
       {state.feed.length === 0 && (
         <>
           <div className="container container--narrow py-md-5">
-        <h2 className="text-center">
-          Hello <strong>{user.username}</strong>, your feed is empty.
-        </h2>
-        <p className="lead text-muted text-center">
-          Your feed displays the latest posts from the people you follow. If you
-          don&rsquo;t have any friends to follow that&rsquo;s okay; you can use
-          the &ldquo;Search&rdquo; feature in the top menu bar to find content
-          written by people with similar interests and then follow them.
-        </p>
-      </div>
+            <h2 className="text-center">
+              Hello <strong>{user.username}</strong>, your feed is empty.
+            </h2>
+            <p className="lead text-muted text-center">
+              Your feed displays the latest posts from the people you follow. If
+              you don&rsquo;t have any friends to follow that&rsquo;s okay; you
+              can use the &ldquo;Search&rdquo; feature in the top menu bar to
+              find content written by people with similar interests and then
+              follow them.
+            </p>
+          </div>
         </>
       )}
     </Page>
